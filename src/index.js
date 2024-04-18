13eb623caa7c4b640a942ee756596e63eecbf0fd
Cszart <bitemecesar@gmail.com>
2024-04-18 15:20:11 -0400

    Styling for movies and movie detail

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { ApolloProvider } from "@apollo/client";
+import client from "./ApolloClient";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
--- src/routes/Movie.js
+++ src/routes/Movie.js
@@ -1,5 +1,76 @@
+import { gql, useQuery } from "@apollo/client";
+import { useParams } from "react-router-dom";
+import { styled } from "styled-components";
+
+const GET_MOVIE_DETAIL = gql`
+  query getMovie($movieId: String!) {
+    movie(id: $movieId) {
+      id
+      title
+      medium_cover_image
+      rating
+    }
+  }
+`;
+
+// --- Style --- //
+const Container = styled.div`
+  height: 100vh;
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  color: white;
+`;
+
+const Column = styled.div`
+  margin-left: 10px;
+  width: 50%;
+`;
+
+const Title = styled.h1`
+  font-size: 65px;
+  margin-bottom: 15px;
+`;
+
+const Subtitle = styled.h4`
+  font-size: 35px;
+  margin-bottom: 10px;
+`;
+
+const Image = styled.div`
+  width: 25%;
+  height: 60%;
+  background-color: transparent;
+  background-image: url(${(props) => props.bg});
+  background-size: cover;
+  background-position: center center;
+  border-radius: 7px;
+`;
+
+// --- Component --- //
 function MovieDetail() {
-  return <div className="MovieDetail">This is movie detail</div>;
+  const { id } = useParams(); // Access movie ID from URL params
+  const { loading, error, data } = useQuery(GET_MOVIE_DETAIL, {
+    variables: { movieId: id },
+  });
+
+  if (loading) return <p>Loading movie details...</p>;
+  if (error) return <p>Error fetching movie details: {error.message}</p>;
+
+  const movie = data?.movie; // Handle potential missing data
+  if (!movie) return <p>Movie not found.</p>;
+
+  return (
+    <Container>
+      <Column>
+        <Title>{loading ? "Loading..." : `${data.movie?.title}`}</Title>
+        <Subtitle>⭐️ {data?.movie?.rating}</Subtitle>
+      </Column>
+      <Image bg={data?.movie?.medium_cover_image} />
+    </Container>
+  );
 }
 
 export default MovieDetail;
